19b029f187a85c774b9cd423b2cd7ef1ffcad63c
Carlos Segarra <carlos@carlossegarra.com>
2023-09-15 18:03:31 +0000

    uk8s: install cluster

--- tasks/kubeadm.py
+++ tasks/kubeadm.py
@@ -6,10 +6,14 @@ from subprocess import run
 from tasks.util.env import (
     BIN_DIR,
     GLOBAL_BIN_DIR,
+    CRI_RUNTIME_SOCKET,
+    FLANNEL_INSTALL_DIR,
     K8S_VERSION,
-    K8S_CONFIG_DIR,
+    K8S_ADMIN_FILE,
+    K8S_CONFIG_FILE,
     K9S_VERSION,
 )
+from time import sleep
 
 
 def _download_binary(url, binary_name):
@@ -88,28 +92,86 @@ def install_k9s(ctx, system=False):
         _symlink_global_bin(binary_path, "k9s")
 
 
+def run_kubectl_command(cmd, capture_output=False):
+    # As long as we don't copy the config file elsewhere we need to use
+    # sudo to read the admin config file
+    k8s_cmd = "sudo kubectl --kubeconfig={} {}".format(K8S_CONFIG_FILE, cmd)
+
+    if capture_output:
+        return run(cmd, shell=True, capture_output=True).stdout.decode("utf-8").strip()
+
+    run(k8s_cmd, shell=True, check=True)
+
+
+def wait_for_pods(ns=None):
+    while True:
+        print("Waiting for pods to be ready...")
+        cmd = [
+            "-n {}".format(ns) if ns else "",
+            "get pods",
+            "-o jsonpath='{..status.conditions[?(@.type==\"Ready\")].status}'",
+        ]
+
+        output = run_kubectl_command(
+            " ".join(cmd),
+            capture_output=True,
+        )
+
+        statuses = [o.strip() for o in output.split(" ") if o.strip()]
+        if all([s == "True" for s in statuses]):
+            print("All pods ready, continuing...")
+            break
+
+        print("Pods not ready, waiting ({})".format(output))
+        sleep(5)
+
+
 @task
-def create(ctx, config_dir=K8S_CONFIG_DIR, clean=False):
+def create(ctx):
     """
     Create a single-node k8s cluster
     """
-    if clean:
-        rmtree(config_dir)
-
-    if not exists(config_dir):
-        makedirs(config_dir)
-
     # Start the cluster
-    kubeadm_config_file = join(config_dir, "kubeadm.conf")
-    kubeadm_cmd = "sudo kubeadm init --config {}".format(kubeadm_config_file)
+    kubeadm_cmd = "sudo kubeadm init --config {}".format(K8S_ADMIN_FILE)
     run(kubeadm_cmd, shell=True, check=True)
 
     # Wait for pods to be ready
+    # TODO
+
+    # Configure flannel
+    run_kubectl_command("apply -f {}".format(join(FLANNEL_INSTALL_DIR, "kube-flannel.yml")))
+    wait_for_pods("kube-flannel")
 
 
 @task
-def destroy(ctx, config=K8S_CONFIG_DIR):
+def destroy(ctx):
     """
     Destroy a k8s cluster initialised with `inv k8s.create`
     """
-    kubeadm_cmd = "kubeadm reset -f --cri-socket='{}'".format(CRI_RUNTIME_SOCKET)
+    def remove_link(dev_name):
+        """
+        Remove link entries from ip tables
+
+        We want to be able to run k8s.destroy multiple times, so we need
+        to spport the link not existing (and the command failing).
+        """
+        ip_cmd = "sudo ip link set dev {} down".format(dev_name)
+        # The command may fail?
+        run(ip_cmd, shell=True)
+        ip_cmd = "sudo ip link del {}".format(dev_name)
+        # The command may fail?
+        run(ip_cmd, shell=True)
+
+    def remove_cni():
+        rmtree("/etc/cni/net.d", ignore_errors=True)
+        remove_link("cni0")
+
+    def remove_flannel():
+        remove_link("flannel.1")
+
+    kubeadm_cmd = "sudo kubeadm reset -f --cri-socket='{}'".format(CRI_RUNTIME_SOCKET)
+    run(kubeadm_cmd, shell=True, check=True)
+
+    # Remove networking stuff
+    remove_cni()
+    remove_flannel()
