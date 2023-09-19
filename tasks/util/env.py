2457c54177ea297a02bc96d75afb141f9f612e9b
Carlos Segarra <carlos@carlossegarra.com>
2023-09-19 18:42:37 +0000

    track kubeadm.conf file

--- tasks/util/env.py
+++ tasks/util/env.py
@@ -6,13 +6,14 @@ BIN_DIR = join(PROJ_ROOT, "bin")
 GLOBAL_BIN_DIR = "/usr/local/bin"
 # See csegarragonz/coco-serverless#3
 GLOBAL_INSTALL_DIR = "/opt"
+CONF_FILES_DIR = join(PROJ_ROOT, "conf-files")
 
 # K8s Config
 
 K8S_VERSION = "1.28.2"
 K9S_VERSION = "0.27.1"
 K8S_CONFIG_DIR = join(PROJ_ROOT, ".config")
-K8S_ADMIN_FILE = join(K8S_CONFIG_DIR, "kubeadm.conf")
+K8S_ADMIN_FILE = join(CONF_FILES_DIR, "kubeadm.conf")
 # TODO: consider copying this file elsewhere
 K8S_CONFIG_FILE = "/etc/kubernetes/admin.conf"
 # This value is hardcoded in ./.config/kubeadm.conf
