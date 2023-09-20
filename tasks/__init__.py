2a83bfe82690278424c954667f5724bf50a689bc
Carlos <carlos@carlossegarra.com>
2023-09-20 11:35:40 -0400

    Knative Hello World (#9)

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -3,19 +3,21 @@ from invoke import Collection
 from . import apps
 from . import containerd
 from . import format_code
-from . import kubeadm
 from . import k8s
 from . import k9s
 from . import kbs
+from . import knative
+from . import kubeadm
 from . import operator
 
 ns = Collection(
     apps,
     containerd,
     format_code,
-    kubeadm,
     k8s,
     k9s,
     kbs,
+    knative,
+    kubeadm,
     operator,
 )
