3bf91e301e77d780edbfc8304ab50ae3ab617e96
Carlos Segarra <carlos@carlossegarra.com>
2023-10-26 22:13:40 +0000

    bin: clean-up the qemu redirection script

--- bin/qemu_wrapper_ovmf_logging.sh
+++ bin/qemu_wrapper_ovmf_logging.sh
@@ -3,11 +3,6 @@
 # This script is a wrapper to modify the QEMU command line when invoked through
 # Kata. We also need to change the Kata config to point the valid hypervisor
 # path to this script
-# TODO: do we need all of this flags? for sure the serial one!
-/opt/confidential-containers/bin/qemu-system-x86_64-csg \
-    -debugcon file:/tmp/ovmf.log \
-    -global isa-debugcon.iobase=0x402 \
+/opt/confidential-containers/bin/qemu-system-x86_64 \
     "$@" \
-    -D /tmp/qemu.log \
-    --trace 'kvm_sev_*' \
     --serial file:/tmp/qemu-serial.log
