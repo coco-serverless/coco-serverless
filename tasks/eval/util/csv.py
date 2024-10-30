ee661d54f256b534ae2a954931b4751b57fdbd77
Carlos Segarra <carlos@carlossegarra.com>
2024-10-30 17:45:39 +0000

    eval: remove from directory

--- tasks/eval/util/csv.py
+++
@@ -1,9 +0,0 @@
-def init_csv_file(file_name, header):
-    with open(file_name, "w") as fh:
-        fh.write("{}\n".format(header))
-
-
-def write_csv_line(file_name, *args):
-    layout = ",".join(["{}" for _ in range(len(args))]) + "\n"
-    with open(file_name, "a") as fh:
-        fh.write(layout.format(*args))
